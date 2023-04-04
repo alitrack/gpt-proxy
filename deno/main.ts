4daa56e4cb6de3eb214d2a54c1cf967a8ae334c2
Steven Lee <admin@alitrack.com>
2023-04-04 04:36:10 +0000

    auto download index.html

--- deno/main.ts
+++ deno/main.ts
@@ -1,6 +1,8 @@
 import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
+import { exists } from "https://deno.land/std@0.182.0/fs/mod.ts";
 
 const OPENAI_API_HOST = "api.openai.com";
+
 
 serve(async (request) => {
     const url = new URL(request.url);
@@ -11,6 +13,15 @@ serve(async (request) => {
             return await fetch(url, request);
             break;
         default:
+            const isReadableFile = await exists("./index.html", {
+                isReadable: true,
+                isFile: true
+            });
+            if (isReadableFile == false) {
+                const res = await fetch("https://raw.githubusercontent.com/xqdoo00o/chatgpt-web/main/index.html");
+                const index = await res.text();
+                await Deno.writeTextFile("index.html", index);
+            }
             return new Response(await Deno.readTextFile("./index.html"), {
                 headers: {
                     "content-type": "text/html;charset=UTF-8",
